8ff4a5a1b84e858a82463b893ade43e5f408bd3b
1onedd1 <48379233+1onedd1@users.noreply.github.com>
2021-10-06 15:38:42 +0300

    change type of sensor

--- sensor/src/main.cpp
+++ sensor/src/main.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include "PressureSensor.cpp"
 
-
 #include "main.h"
 using namespace std;
 
 int main() {
-	PressureSensor ps("Pressure");
+	PressureSensor ps("Induction");
 	ps.powerOn();
 	cout << "Sensor value = " << ps.getValue() << endl;
 	cout << "Sensor type = " << ps.getType() << endl;
