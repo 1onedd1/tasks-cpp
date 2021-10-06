a032bd5956aa0cf21cc11c1ff4446586f313ab81
1onedd1 <48379233+1onedd1@users.noreply.github.com>
2021-10-06 15:41:34 +0300

    try to impl

--- employeedb/src/main.cpp
+++ employeedb/src/main.cpp
@@ -1,9 +1,19 @@
+#include <stdlib.h>
 #include <iostream>
-#include <string>
+
+#include "mysql_connection.h"
+
+#include <driver.h>
+#include <exception.h>
+#include <resultset.h>
+#include <statement.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "asd";
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* statement;
+    sql::ResultSet* res;
 }
